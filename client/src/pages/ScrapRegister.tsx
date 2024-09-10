c8ac6fd957eba959f20feb33f1fb6bcbc1fccee3
apps smi <smiapps@smimx.net>
2024-09-10 12:36:35 -0700

    before migrate

--- client/src/pages/ScrapRegister.tsx
+++ client/src/pages/ScrapRegister.tsx
@@ -323,7 +323,7 @@ const ScrapRegister: React.FC = () => {
     };
 
     return (
-        <div className='flex flex-col px-7 py-4 md:px-10 md:py-6 bg-[#d7d7d7] h-full sm:h-screen'>
+        <div className='flex flex-col px-7 py-4 md:px-10 md:py-6 bg-[#d7d7d7] h-screen sm:h-screen'>
             <ToastContainer
                 position='top-center'
                 autoClose={false}
