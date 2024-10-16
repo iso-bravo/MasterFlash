a19bf36a890745210d2bfc02e0562e61a8a42095
Osminalx <verdestro64@gmail.com>
2024-10-16 11:45:50 -0700

    Part num cataloge search bar added

--- client/src/pages/PartNumCataloge.tsx
+++ client/src/pages/PartNumCataloge.tsx
@@ -3,11 +3,12 @@ import { IoIosArrowBack } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 import api from '../config/axiosConfig';
 import { FaPlus } from 'react-icons/fa';
+import { IoSearch } from 'react-icons/io5';
 
 interface PartNum {
     part_number: string | null;
     box: string | null;
-    client:string | null; 
+    client: string | null;
     pieces_x_box: number | null;
     rubber_compound: string | null;
     standard: number | null;
@@ -23,6 +24,8 @@ interface PartNum {
 const PartNumCataloge = () => {
     const navigate = useNavigate();
     const [partNums, setPartNums] = useState<PartNum[]>([]);
+    const [filteredPartNums, setFilteredPartNums] = useState<PartNum[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(true);
 
     const fetchPartNums = async () => {
@@ -32,13 +35,21 @@ const PartNumCataloge = () => {
         } catch (error) {
             console.error('Error obteniendo datos: ', error);
         } finally {
-            setLoading(false); // Cambia el estado a false cuando los datos ya fueron obtenidos
+            setLoading(false);
         }
     };
 
     useEffect(() => {
         fetchPartNums();
     }, []);
+
+    useEffect(()=>{
+        const filtered = partNums.filter(item=>
+            item.part_number?.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+        );
+        setFilteredPartNums(filtered)
+
+    },[searchTerm,partNums])
 
     return (
         <div className='flex flex-col px-7 py-4 md:px-10 md:py-6 bg-[#d7d7d7] h-full sm:h-screen'>
@@ -47,6 +58,24 @@ const PartNumCataloge = () => {
                 <h1 className='text-xl'>Catálogo de Números de Parte</h1>
             </header>
             <section className='flex justify-end'>
+                <div className='m-2'>
+                    <label htmlFor='search' className='mb-2 text-sm font-medium text-gray-900 sr-only'>
+                        Número de parte
+                    </label>
+                    <div className='relative'>
+                        <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
+                            <IoSearch />
+                        </div>
+                        <input
+                            id='search'
+                            type='text'
+                            value={searchTerm}
+                            onChange={e => setSearchTerm(e.target.value)}
+                            className='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500'
+                            placeholder='Buscar Números de parte'
+                        />
+                    </div>
+                </div>
                 <button
                     className='flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
                     onClick={() => navigate('/part_num_creation')}
@@ -104,7 +133,7 @@ const PartNumCataloge = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {partNums.map((item, index) => (
+                            {filteredPartNums.map((item, index) => (
                                 <tr key={index} className='odd:bg-white even:bg-gray-50 border-b'>
                                     <th className='px-6 py-3'>{item.part_number}</th>
                                     <th className='px-6 py-3'>{item.client}</th>
