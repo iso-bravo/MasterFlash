d5ecafc9a9e3497d29ef1067a3a850ec779fe67b
Osminalx <verdestro64@gmail.com>
2024-11-25 12:28:23 -0800

    Change all Part Number Ugly UI

--- client/src/components/EditPartNumModal.tsx
+++ client/src/components/EditPartNumModal.tsx
@@ -1,48 +1,89 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { IoClose } from 'react-icons/io5';
+import api from '../config/axiosConfig';
 
 interface PartNum {
     id: number;
-    part_number: string | null;
-    box: string | null;
-    client: string | null;
-    pieces_x_box: number | null;
-    rubber_compound: string | null;
-    standard: number | null;
-    pallet: string | null;
-    box_x_pallet: number | null;
-    pieces_x_pallet: number | null;
-    mold: string | null;
-    insert: string | null;
-    caliber: string | null;
-    gripper: string | null;
+    part_number: string;
+    client: string;
+    box: string;
+    pieces_x_box: number;
+    rubber_compound: string;
+    price?: number;
+    standard: number;
+    pallet: string;
+    box_x_pallet: number;
+    pieces_x_pallet?: number;
+    assembly?: string;
+    accessories?: string;
+    mold: string;
+    instructive?: string;
+    insert: string;
+    gripper: string;
+    caliber: string;
+    paint?: string;
+    std_paint?: number;
+    painter?: number;
+    scrap?: number;
+    box_logo?: string;
+    cavities?: number;
+    category?: string;
+    type2?: string;
+    measurement?: string;
+    special?: string;
+    piece_label?: string;
+    qty_piece_labels?: number;
+    box_label?: string;
+    qty_box_labels?: number;
+    box_label_2?: string;
+    qty_box_labels_2?: number;
+    box_label_3?: string;
+    qty_box_labels_3?: number;
+    made_in_mexico?: string;
+    staples?: string;
+    image_piece_label?: File | null;
+    image_box_label?: File | null;
+    image_box_label_2?: File | null;
+    image_box_label_3?: File | null;
 }
 
 interface EditPartNumModalProps {
-    partNum: PartNum | null;
+    partNumId: number | null;
     isOpen: boolean;
     onClose: () => void;
-    onSave: (updatedPartNum: PartNum) => void;
+    onSave: (updatedPartNum: PartNum) => Promise<void>;
 }
 
-const EditPartNumModal: React.FC<EditPartNumModalProps> = ({ partNum, isOpen, onClose, onSave }) => {
+const EditPartNumModal: React.FC<EditPartNumModalProps> = ({ partNumId, isOpen, onClose, onSave }) => {
     const {
         register,
         handleSubmit,
         reset,
         formState: { errors },
-    } = useForm<PartNum>({
-        defaultValues: partNum || {},
-    });
+    } = useForm<PartNum>();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [partNumData, setPartNumData] = useState<PartNum | null>(null);
+    
 
     useEffect(() => {
-        if (partNum) {
-            reset(partNum);
-        } else {
-            reset({});
+        if (isOpen && partNumId) {
+            setLoading(true);
+
+            api.get(`/part_numbers/${partNumId}/`)
+                .then(response => {
+                    setPartNumData(response.data);
+                    reset(response.data);
+                    setError(null);
+                })
+                .catch(err => {
+                    console.error(err);
+                    setError('Error al cargar los datos del número de parte.');
+                })
+                .finally(() => setLoading(false));
         }
-    }, [partNum, reset]);
+    }, [isOpen, partNumId, reset]);
 
     if (!isOpen) return null;
 
@@ -58,107 +99,54 @@ const EditPartNumModal: React.FC<EditPartNumModalProps> = ({ partNum, isOpen, on
                     <h2 className='text-xl font-bold'>Editar Número de Parte</h2>
                     <IoClose size={30} className='cursor-pointer' onClick={onClose} />
                 </div>
-                <form onSubmit={handleSubmit(onSubmit)} className='space-y-4 mt-4'>
-                    {/* Campos */}
-                    <div>
-                        <label className='block mb-1'>Número de Parte</label>
-                        <input
-                            {...register('part_number', { required: 'Este campo es obligatorio' })}
-                            className={`w-full p-2 border rounded ${errors.part_number ? 'border-red-500' : ''}`}
-                            placeholder='Número de Parte'
-                        />
-                        {errors.part_number && <p className='text-red-500 text-sm'>{errors.part_number.message}</p>}
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Box</label>
-                        <input {...register('box')} className='w-full p-2 border rounded' placeholder='Box' />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Cliente</label>
-                        <input {...register('client')} className='w-full p-2 border rounded' placeholder='Cliente' />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Piezas por Caja</label>
-                        <input
-                            type='number'
-                            {...register('pieces_x_box')}
-                            className='w-full p-2 border rounded'
-                            placeholder='Piezas por Caja'
-                        />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Compuesto de Goma</label>
-                        <input
-                            {...register('rubber_compound')}
-                            className='w-full p-2 border rounded'
-                            placeholder='Compuesto de Goma'
-                        />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Estándar</label>
-                        <input
-                            type='number'
-                            {...register('standard')}
-                            className='w-full p-2 border rounded'
-                            placeholder='Estándar'
-                        />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Pallet</label>
-                        <input
-                            type='number'
-                            {...register('pallet')}
-                            className='w-full p-2 border rounded'
-                            placeholder='Estándar'
-                        />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Box X Pallet</label>
-                        <input
-                            type='number'
-                            {...register('box_x_pallet')}
-                            className='w-full p-2 border rounded'
-                            placeholder='Estándar'
-                        />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Piezas X Pallet</label>
-                        <input
-                            type='number'
-                            {...register('pieces_x_pallet')}
-                            className='w-full p-2 border rounded'
-                            placeholder='Estándar'
-                        />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Molde</label>
-                        <input {...register('mold')} className='w-full p-2 border rounded' placeholder='Mold' />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Inserto</label>
-                        <input {...register('insert')} className='w-full p-2 border rounded' placeholder='Mold' />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Calibre</label>
-                        <input {...register('caliber')} className='w-full p-2 border rounded' placeholder='Mold' />
-                    </div>
-                    <div>
-                        <label className='block mb-1'>Gripper</label>
-                        <input {...register('gripper')} className='w-full p-2 border rounded' placeholder='Mold' />
-                    </div>
-                    <div className='flex justify-end space-x-2 mt-6'>
-                        <button
-                            type='button'
-                            onClick={onClose}
-                            className='px-4 py-2 bg-gray-300 rounded hover:bg-gray-400'
-                        >
-                            Cancelar
-                        </button>
-                        <button type='submit' className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'>
-                            Guardar
-                        </button>
-                    </div>
-                </form>
+
+                {loading && <p className='text-gray-500'>Cargando datos...</p>}
+                {error && <p className='text-red-500'>{error}</p>}
+
+                {!loading && (
+                    <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-4 gap-4'>
+                        {partNumData &&
+                            Object.keys(partNumData).map(key => (
+                                <div key={key} className='col-span-1'>
+                                    <label className='block mb-1 capitalize'>{key.replace(/_/g, ' ')}</label>
+                                    {key.startsWith('image_') ? (
+                                        <input
+                                            type='file'
+                                            {...register(key as keyof PartNum)}
+                                            className='w-full p-2 border rounded'
+                                        />
+                                    ) : (
+                                        <input
+                                            type='text'
+                                            {...register(key as keyof PartNum)}
+                                            className='w-full p-2 border rounded'
+                                            placeholder={key.replace(/_/g, ' ')}
+                                        />
+                                    )}
+                                    {errors[key as keyof PartNum] && (
+                                        <p className='text-red-500 text-sm'>Error en {key.replace(/_/g, ' ')}</p>
+                                    )}
+                                </div>
+                            ))}
+
+                        {/* Botones de acción */}
+                        <div className='col-span-2 flex justify-end space-x-4 mt-6'>
+                            <button
+                                type='button'
+                                onClick={onClose}
+                                className='px-4 py-2 bg-gray-300 rounded hover:bg-gray-400'
+                            >
+                                Cancelar
+                            </button>
+                            <button
+                                type='submit'
+                                className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'
+                            >
+                                Guardar
+                            </button>
+                        </div>
+                    </form>
+                )}
             </div>
         </div>
     );
