4c2db215cacdf477ca09d965eb449d877982c532
Osminalx <verdestro64@gmail.com>
2025-01-29 13:19:09 -0800

    Anual production graph

--- client/src/types/DashBoardTypes.ts
+++ client/src/types/DashBoardTypes.ts
@@ -1,27 +1,33 @@
 
 export interface scrapData {
-    molder_number: string;
-    CS?: number;
-    CROP?: number;
-    DP?: number;
-    DI?: number;
-    F?: number;
-    FC?: number;
-    FPO?: number;
-    GA?: number;
-    GM?: number;
-    H?: number;
-    IM?: number;
-    IMC?: number;
-    IR: number;
-    M?: number;
-    MR?: number;
-    R?: number;
-    SG?: number;
-    SI?: number;
+  molder_number: string;
+  CS?: number;
+  CROP?: number;
+  DP?: number;
+  DI?: number;
+  F?: number;
+  FC?: number;
+  FPO?: number;
+  GA?: number;
+  GM?: number;
+  H?: number;
+  IM?: number;
+  IMC?: number;
+  IR: number;
+  M?: number;
+  MR?: number;
+  R?: number;
+  SG?: number;
+  SI?: number;
 }
 
 export interface WeekProduction {
-    day: string;
-    pieces_ok: number;
+  day: string;
+  pieces_ok: number;
+}
+
+export interface AnualProduction {
+month: string;
+goal: number;
+pieces_ok: number;
 }
